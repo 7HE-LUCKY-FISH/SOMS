f946b661214362eddeac57232a286f03c47c46b7
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2025-09-30 15:19:43 -0700

    adding scripts for DDL and .venv/uv requiremnts.txt

---
+++ scripts.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import Error
+import os
+import time
+
+for _ in range(10):
+    try:
+        mydb = mysql.connector.connect(
+            host=os.getenv('DB_HOST', 'localhost'),
+            user=os.getenv('DB_USER', 'root'),   #we can change to port vs socket if needed
+            password=os.getenv('DB_PASSWORD', 'adminpass'),#change password here or grba from env
+            auth_plugin='mysql_native_password'
+        )
+        break
+    except Error:
+        print("Waiting for database connection...")
+        time.sleep(5)
+else:
+    print("Could not connect to the database.")
+    exit(1)
+
+
+
+cursor = mydb.cursor()
