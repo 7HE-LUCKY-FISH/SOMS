3da30a0031e358c0fae454fddb2a4448c8d147f4
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2025-10-02 11:34:53 -0700

    adding to DDL

--- SOMS_db_create.py
+++ SOMS_db_create.py
@@ -27,3 +27,28 @@ cursor.execute("CREATE DATABASE IF NOT EXISTS SOMS")
 cursor.execute("USE SOMS")
 
 #DDL to create tables
+
+
+cursor.execute("""
+    create table if not exists staff
+    ( staff_id int auto_increment primary key,
+        first_name varchar(50) not null,
+        middle_name varchar(50),
+        last_name varchar(50) not null,
+        email varchar(100) not null unique,
+        salary decimal(10,2) not null,
+        age int not null,
+        date_hired date not null default(current_date)
+    )
+    """)
+
+cursor.execute("""
+    create table if not exists coach(
+        staff_id int auto_increment primary key,
+        role varchar(50) not null
+    )
+    """)
+
+#  CONSTRAINT fk_coach
+#    FOREIGN KEY (staff_id) REFERENCES staff(staff_id) ON DELETE CASCADE
+#idk what the hell we want here lol :cry:
