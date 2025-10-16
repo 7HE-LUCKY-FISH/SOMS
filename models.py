2f3054eca0dc64c5dc6c4da627719e52eda72043
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2025-10-16 12:03:30 -0700

    adding the default classes: Might change later depending on FastAPI creation and frontend db

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from db_connect_module import get_db_connection
 
 
 #do we want login , sanitization, validation, error handling etc
-
+# there is optimization as python classes are slow
 class Staff:
     def __init__(self, first_name, middle_name, last_name, email, salary, age, staff_type):
         self.first_name = first_name
@@ -27,6 +27,72 @@ class Staff:
         cursor.close()
         conn.close()
 
+class Coach:
+    def __init__(self, staff_id, role, team_id=None):
+        self.staff_id = staff_id
+        self.role = role
+        self.team_id = team_id
 
+    @staticmethod
+    def create(coach):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = """
+        INSERT INTO coach (staff_id, role, team_id)
+        VALUES (%s, %s, %s)
+        """
+        cursor.execute(query, (coach.staff_id, coach.role, coach.team_id))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
-#can prob do this later if we add more depending on the framework 
+class Player:
+    def __init__(self, first_name, middle_name, last_name, salary, positions, 
+                 is_active=True, is_injured=False, transfer_value=None, 
+                 contract_end_date=None, scouted_player=False):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.salary = salary
+        self.positions = positions
+        self.is_active = is_active
+        self.is_injured = is_injured
+        self.transfer_value = transfer_value
+        self.contract_end_date = contract_end_date
+        self.scouted_player = scouted_player
+
+    @staticmethod
+    def create(player):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = """
+        INSERT INTO player (first_name, middle_name, last_name, salary, positions, 
+                           is_active, is_injured, transfer_value, contract_end_date, scouted_player)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(query, (player.first_name, player.middle_name, player.last_name, 
+                               player.salary, player.positions, player.is_active, 
+                               player.is_injured, player.transfer_value, player.contract_end_date, 
+                               player.scouted_player))
+        conn.commit()
+        cursor.close()
+        conn.close() 
+
+class Scout:
+    def __init__(self, staff_id, region, YOE):
+        self.staff_id = staff_id
+        self.region = region
+        self.YOE = YOE
+    
+    @staticmethod
+    def create(scout):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = """
+        INSERT INTO scout (staff_id, region, YOE)
+        VALUES (%s, %s, %s)
+        """
+        cursor.execute(query, (scout.staff_id, scout.region, scout.YOE))
+        conn.commit()
+        cursor.close()
+        conn.close()
